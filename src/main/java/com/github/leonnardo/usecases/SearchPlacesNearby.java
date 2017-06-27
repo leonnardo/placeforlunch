f0adbc2ce0f16e4914c4856eaeb8fbcc969d1aff
Leonnardo Rabello <leonnardo.rabello@netshoes.com>
2017-06-27 18:44:49 -0300

    All places result almost working

--- src/main/java/com/github/leonnardo/usecases/SearchPlacesNearby.java
+++ src/main/java/com/github/leonnardo/usecases/SearchPlacesNearby.java
@@ -24,19 +24,30 @@ public class SearchPlacesNearby {
 
     public List<RestaurantResponse> search(final Integer distance, final Double lat, final Double lng) {
         final LatLng coordinates = new LatLng(lat, lng);
-        NearbySearchRequest searchRequest = PlacesApi.nearbySearchQuery(context, coordinates).type(PlaceType.RESTAURANT)
-                .rankby(RankBy.DISTANCE);
 
-        PlacesSearchResponse response = searchRequest.awaitIgnoreError();
+        PlacesSearchResponse res;
+        String pageToken = "";
+        final ArrayList<PlacesSearchResult> placesSearchResults = new ArrayList<>();
+        do {
+            NearbySearchRequest searchRequest = PlacesApi.nearbySearchQuery(context, coordinates)
+                    .type(PlaceType.RESTAURANT)
+                    .rankby(RankBy.DISTANCE)
+                    .pageToken(pageToken);
 
-        return convertFromPlacesToJson(Arrays.asList(response.results));
+            res = searchRequest.awaitIgnoreError();
+            placesSearchResults.addAll(Arrays.asList(res.results));
+            pageToken = Optional.ofNullable(res.nextPageToken).orElse("");
+
+        } while (res.nextPageToken != null);
+
+        return convertFromPlacesToJson(placesSearchResults);
     }
 
 
     private List<RestaurantResponse> convertFromPlacesToJson(List<PlacesSearchResult> placesSearchResults) {
         final ArrayList<RestaurantResponse> response = new ArrayList<>();
         placesSearchResults.forEach(place -> response.add(new RestaurantResponse(place.name, place.vicinity, (double) place.rating)));
-        response.sort(Comparator.comparing(RestaurantResponse::getRating).reversed());
+        //response.sort(Comparator.comparing(RestaurantResponse::getRating).reversed());
         return response;
     }
 }
